ea3caadfed72373f9ca43071a005817e9d56de7b
Barotti-Dragneel <gustavo.barotti@pdbomdespacho.com.br>
2025-05-04 19:27:46 -0300

    Add files via upload

---
+++ Módulo 2/aula2_questao2.py
@@ -0,0 +1,12 @@
+valor = "O valor é:"
+x = 10
+y = 3.5
+z = (x + y)
+print (valor)
+print (type (valor))
+print (x)
+print (type (x))
+print (y)
+print (type (y))
+print (z)
+print (type (z))
---
+++ Módulo 2/aula2_questao3.py
@@ -0,0 +1,7 @@
+v1 = 10
+v2 = 20
+vx = v1
+v1 = v2
+v2 = vx
+print (v1)
+print (v2)
